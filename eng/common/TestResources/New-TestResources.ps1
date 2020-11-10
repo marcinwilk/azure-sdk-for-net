1e0734ceaf9fa5ee7a11fa5e9c1e06e95107281a
Ben Broderick Phillips <bebroder@microsoft.com>
2020-11-10 06:39:24 -0500

    remove plat

--- eng/common/TestResources/New-TestResources.ps1
+++ eng/common/TestResources/New-TestResources.ps1
@@ -333,7 +333,7 @@ elseif (($resourceGroup -eq $null) -and (-not $PSCmdlet.ShouldProcess($resourceG
 
 # Populate the template parameters and merge any additional specified.
 $templateParameters = @{
-    baseName = $BaseName
+    baseName = "benbp-basename"
     testApplicationId = $TestApplicationId
     testApplicationOid = "$TestApplicationOid"
 }
@@ -385,13 +385,15 @@ foreach ($templateFile in $templateFiles) {
 
     Log "Deploying template '$templateFile' to resource group '$($resourceGroup.ResourceGroupName)'"
     $deployment = Retry {
-        New-AzResourceGroupDeployment `
-            -Name $BaseName `
-            -ResourceGroupName $resourceGroup.ResourceGroupName `
-            -TemplateFile $templateFile `
-            -TemplateParameterObject $templateFileParameters `
-            -Force:$Force `
-            -Debug
+        $lastDebugPreference = $DebugPreference
+        try {
+            $DebugPreference = "Continue"
+            New-AzResourceGroupDeployment -Name $BaseName -ResourceGroupName $resourceGroup.ResourceGroupName -TemplateFile $templateFile -TemplateParameterObject $templateFileParameters
+        } catch {
+            throw $_
+        } finally {
+            $DebugPreference = $lastDebugPreference
+        }
     }
 
     if ($deployment.ProvisioningState -eq 'Succeeded') {
